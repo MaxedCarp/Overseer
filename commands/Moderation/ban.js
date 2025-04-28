144be8162b43d5439edd782b3714ad1830d99604
MaxedCarp <60354312+MaxedCarp@users.noreply.github.com>
2025-04-28 16:04:46 +0300

    Created the /ban command

---
+++ commands/Moderation/ban.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+const EmbedCreator = require('../../Event_Modules/embedcreator.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ban')
+		.setDescription('Ban a user')
+		.addUserOption(option =>
+            option.setName('user')
+                .setDescription('User to ban')
+				.setRequired(true))
+		.addStringOption(option =>
+            option.setName('reason')
+                .setDescription('Reason'))
+		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+	async execute(interaction) {
+		const member = interaction.options.getMember('user');
+		const user = member.user;
+		const reason = interaction.options.getString('reason');
+		if (!member.roles.highest.editable) {
+			await interaction.reply({ content: `You can not ban this user!`, ephemeral: true });
+			return;
+		}
+		await member.ban();
+		await interaction.reply({ content: `User: ${member.user} banned successfully for: ${reason}`});
+		const msgobj = { srv: interaction.guild.id, userID: user.id, username: user.username, noteAuthor: { userID: interaction.user.id, userName: interaction.user.username, globalName: interaction.user.globalName, avatar: interaction.user.avatar, avatarURL: interaction.user.displayAvatarURL() }, type: "ban", text: `${reason || "No reason provided."}.`};
+		await global.notecol.insertOne(msgobj);
+		let obj = await global.srvcol.findOne({ "srv": interaction.guild.id});
+		let resembed = await EmbedCreator.Create(`Moderation Command executed in: <#${interaction.channel.id}>`, `Command: /ban\nTarget User: ${member.user}.`, false, interaction.guild.name, interaction.guild.iconURL(), `${interaction.user.globalName || interaction.user.username} (${interaction.user.username})`, `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}`, 0xff9900, []);
+		if (obj.delete === "none" || !obj)
+			return;
+		if (((interaction.guild.members.me).permissionsIn(obj.moderationlog).has(PermissionFlagsBits.SendMessages) && (interaction.guild.members.me).permissionsIn(obj.moderationlog).has(PermissionFlagsBits.ViewChannel)) || (interaction.guild.members.me).permissionsIn(obj.moderationlog).has(PermissionFlagsBits.Administrator))
+			await client.channels.cache.get(obj.moderationlog).send({ embeds: [resembed] });
+		else
+			return;
+	},
+};
