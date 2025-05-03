6e751fa14c07e35b9e797f51c9b0247b8f86cbbc
MaxedCarp <60354312+MaxedCarp@users.noreply.github.com>
2025-05-03 18:30:20 +0300

    Added a Serial number field to the Note system, and updated all moderation commands to automatically add a serial number to each note.

--- commands/Moderation/note.js
+++ commands/Moderation/note.js
@@ -8,7 +8,7 @@ module.exports = {
 		.addSubcommand(subcommand =>
 		subcommand
 			.setName('add')
-			.setDescription('Adds a new role to be given on join.')
+			.setDescription('Write a note to the specified user.')
 			.addUserOption(option =>
             option.setName('user')
                 .setDescription('Target user')
@@ -19,11 +19,11 @@ module.exports = {
 				.setRequired(true)))
 		.addSubcommand(subcommand =>
 		subcommand
-			.setName('remove')
-			.setDescription('Remove a join role.')
+			.setName('delete')
+			.setDescription('Delete a note by ID.')
 			.addIntegerOption(option =>
-            option.setName('index')
-                .setDescription('Index of the role to be removed')
+            option.setName('id')
+                .setDescription('The ID of the note you wish to remove.')
 				.setRequired(true)))
 		.addSubcommand(subcommand =>
 		subcommand
@@ -38,15 +38,20 @@ module.exports = {
 		const sub = interaction.options.getSubcommand();
 		const user = interaction.options.getUser('user');
 		const note = interaction.options.getString('note');
-		const index = interaction.options.getInteger('index');
+		const index = interaction.options.getInteger('id');
 		switch(sub) {
 			case "add":
-				const msgobj = { srv: interaction.guild.id, userID: user.id, username: user.username, noteAuthor: { userID: interaction.user.id, userName: interaction.user.username, globalName: interaction.user.globalName, avatar: interaction.user.avatar, avatarURL: interaction.user.displayAvatarURL() }, type: "note", text: note.replace("\\n","\n")};
+				const msgobj = { srv: interaction.guild.id, userID: user.id, username: user.username, noteAuthor: { userID: interaction.user.id, userName: interaction.user.username, globalName: interaction.user.globalName, avatar: interaction.user.avatar, avatarURL: interaction.user.displayAvatarURL() }, type: "note", text: note.replace("\\n","\n"), serial: ((await global.notecol.find().sort({"serial": -1}).toArray())[0].serial) + 1};
 				await global.notecol.insertOne(msgobj);
 				await interaction.reply({ content: `Successfully added note to user: ${user.username}`, ephemeral: true });
 			break;
-			case "remove":
-				await interaction.reply({ content: `The target user has no notes.`, ephemeral: true });
+			case "delete":
+				if (await global.notecol.count({serial: index, srv: interaction.guild.id}) > 0) {
+					await global.notecol.deleteOne({serial: index, srv: interaction.guild.id});
+					await interaction.reply({content: `Note #${index} deleted successfully.`, ephemeral: true});
+				}
+				else
+					await interaction.reply({content: `Either note #${index} does not exist or you have no permission to delete it.`, ephemeral: true});
 			break;
 			case "list":
 				const notelist = new EmbedBuilder()
@@ -54,13 +59,13 @@ module.exports = {
 				.setTitle(`${user.username}'s notes`)
 				.setThumbnail(user.displayAvatarURL())
 				.setAuthor({ name: `${interaction.user.username}`, iconURL: `${interaction.member.displayAvatarURL()}` })
-				.setFooter({ text: interaction.guild.name, iconURL: interaction.guild.iconURL() });
+				.setFooter({ text: `${interaction.guild.name}`, iconURL: interaction.guild.iconURL() });
 				var list = "";
 				const data = await global.notecol.find({srv: interaction.guild.id, userID: user.id}).toArray();
-				if (data.length > 0) {
+				if (await global.notecol.count({srv: interaction.guild.id, userID: user.id}) > 0){
 					i = 1;
 					data.forEach(note => {
-						list += `(${i}).\n- Note Type: ${note.type}.\n- Issued by: <@${note.noteAuthor.userID}>.\n${note.text}.\n\n`;
+						list += `-# \\|\\|NOTE ID:${note.serial}\\|\\|\n- Note Type: ${note.type}.\n- Issued by: <@${note.noteAuthor.userID}>.\n${note.text}.\n\n`;
 						i++;
 					});
 					notelist.setDescription(list);
