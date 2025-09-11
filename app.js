ddd9b020a0af4211db8106b2c85166f40f006059
MaxedCarp <60354312+MaxedCarp@users.noreply.github.com>
2025-09-11 20:24:56 +0300

    Fixed a few bugs involving user counts in channels for the one-time access system.

--- app.js
+++ app.js
@@ -206,14 +206,14 @@ eventEmitter.on('channelsCheckStart', async () => {
                     }).toArray();
                     if (!!overwrites.length > 0) {
                         if (await (channel.permissionOverwrites.cache).find(exp => exp.type === 1) && channel.members.size < 1) {
-                            for (overwrite of overwrites) {
+                            for (o of overwrites) {
                                 const members = await guild.members.fetch();
-                                const member = await members.find(m => m.id === overwrite.userID);
+                                const member = await members.find(m => m.id === o.userID);
                                 await channel.permissionOverwrites.delete(member.user);
                                 await global.channelscol.deleteOne({
                                     "srv": guild.id,
                                     "channelID": channel.id,
-                                    "userID": overwrite.userID
+                                    "userID": o.userID
                                 })
                             }
                         }
