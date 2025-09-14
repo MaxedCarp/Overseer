39235535371bef1b664015c4bb395d13b04460d3
MaxedCarp <60354312+MaxedCarp@users.noreply.github.com>
2025-09-14 14:59:01 +0300

    Added /movetochannel

---
+++ commands/Channels/moveall.js
@@ -0,0 +1,27 @@
+const {SlashCommandBuilder, PermissionFlagsBits, ChannelType} = require('discord.js');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('movealltochannel')
+        .setDescription('Grant a user access to a channel for a limited time')
+        .addChannelOption(option =>
+            option.setName('channel')
+                .setDescription('Voice channel to move to')
+                .addChannelTypes(ChannelType.GuildVoice)
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+    async execute(interaction) {
+        if (!interaction.member.voice.channel) {
+            await interaction.reply({
+                content: `You must be in a voice channel to use this command!`,
+                ephemeral: true
+            });
+            return;
+        }
+        const channel = interaction.options.getChannel('channel');
+        console.log(channel.permissionOverwrites.cache);
+        await interaction.member.voice.channel.members.forEach((member) => {
+            member.setChannel(channel);
+        });
+        await interaction.reply({content: `Successfully moved all users to channel: ${channel.name} `, ephemeral: true});
+    },
+};
