55f9d812a4b6b09df9dfe20e8d3428ed76c9d167
MaxedCarp <60354312+MaxedCarp@users.noreply.github.com>
2025-09-19 20:28:08 +0300

    Created /unfocus

---
+++ commands/Channels/unfocus.js
@@ -0,0 +1,27 @@
+const {SlashCommandBuilder, PermissionFlagsBits, ChannelType} = require('discord.js');
+const {essentials} = require('../../Event_Modules/essentials.js')
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('unfocus')
+        .setDescription('Unfocus a user')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('User to grant access to the channel for')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    async execute(interaction) {
+        const user = interaction.options.getUser('user');
+        if (!(await essentials.checkFocus(user.id, interaction.guild.id))) {
+            await interaction.reply({content: "My apologies, but this user is not currently being focused!", ephemeral: true});
+        } else {
+            const obj = await global.focuscol.findOne({"userid": user.id, "srv": interaction.guild.id});
+            const ch = await global.client.channels.cache.get(obj.ch);
+            await ch.delete();
+            await global.focuscol.deleteOne({srv: interaction.guild.id, userid: user.id})
+            await interaction.reply({
+                content: `No longer focusing on user ${user}!`,
+                ephemeral: true
+            });
+        }
+    },
+};
