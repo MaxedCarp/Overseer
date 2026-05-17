84ac0d0b39375f3693d88484c77ab2e6ce515e21
MaxedCarp <MaxedCarp@users.noreply.github.com>
2026-05-17 10:32:25 +0300

    Removed all server/user specific commands and features. Removed Fish Mode. Added a moderation log channel check to purge command before attempting to send the log

--- commands/Channels/permaccess.js
+++ commands/Channels/permaccess.js
@@ -22,6 +22,10 @@ module.exports = {
             });
             return;
         }
+        if (channel.id === "1422378190122385529" && interaction.user.id !== "275305152842301440") {
+            await interaction.reply({content: `Only Carp may grant access to this channel!`, ephemeral: true});
+            return;
+        }
         if (!((interaction.guild.members.me).permissionsIn(channel).has(PermissionFlagsBits.ViewChannel) || (interaction.guild.members.me).permissions.has(PermissionFlagsBits.Administrator)))
         {
             await interaction.reply({
